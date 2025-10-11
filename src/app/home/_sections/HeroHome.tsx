4b04941e505cc130c39b5c95547cbcb57b9192af
Marsad Ghanvi <marsad11223@gmail.com>
2025-10-12 02:19:53 +0500

    section HeroHome

---
+++ src/app/home/_sections/HeroHome.tsx
@@ -0,0 +1,89 @@
+"use client";
+import React from "react";
+import { localFontSize, sectionPadding } from "@/utils/themes";
+import { Box, Typography } from "@mui/material";
+import CustomButton from "@/_components/CustomButton";
+
+export default function HeroHome() {
+  return (
+    <Box
+      data-aos="zoom-in"
+      data-aos-duration="500"
+      sx={{
+        backgroundImage: "url(bgHome.webp)",
+        backgroundPosition: { xs: "center", sm: "center", md: "cover" },
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        display: "flex",
+        minHeight: { xs: "calc( 100vh - 75px )", md: "700px", lg: "800px" },
+      }}
+    >
+      <Box
+        sx={{
+          maxWidth: "1440px",
+          width: "100%",
+          margin: "auto",
+          padding: sectionPadding,
+        }}
+      >
+        <Box>
+          <Box
+            sx={{
+              maxWidth: { xs: "500px", sm: "600px", md: "780px" },
+              width: "100%",
+              textAlign: "left",
+            }}
+          >
+            <Typography
+              sx={{
+                fontSize: localFontSize.h1,
+                fontWeight: "700",
+                textTransform: "uppercase",
+                color: "#fff",
+                marginBottom: { xs: "10px", md: "14px" },
+                // elipsis
+                display: "-webkit-box",
+                WebkitLineClamp: { md: 3 },
+                WebkitBoxOrient: { md: "vertical" },
+                overflow: { md: "hidden" },
+                textOverflow: { md: "ellipsis" },
+              }}
+            >
+              Empowering Muslim Professionals, Together
+            </Typography>
+
+            <Typography
+              sx={{
+                fontSize: localFontSize.p2,
+                fontWeight: "400",
+                color: "#FFFFFF80",
+                // elipsis
+                display: "-webkit-box",
+                WebkitLineClamp: 4,
+                WebkitBoxOrient: "vertical",
+                overflow: "hidden",
+                textOverflow: "ellipsis",
+              }}
+            >
+              At its core, HOW gathers Muslim industry leaders from across
+              society4politics, finance, law, academia, media, medicine,
+              business, and tech4to create a powerful space
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              marginTop: { xs: "20px", md: "40px" },
+              display: "flex",
+              gap: { xs: "10px", sm: "20px" },
+              flexDirection: { xs: "column", sm: "row" },
+              width: "fit-content",
+            }}
+          >
+            <CustomButton text="get involved" lightmode={true} />
+            <CustomButton text="meet our people" />
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
